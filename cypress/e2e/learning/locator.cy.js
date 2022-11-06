2f331a05c5a9887dc125cfe87301ae9de7fabc0b
Paulina Karaś <ppaulina.karas@gmail.com>
2022-11-06 15:41:51 +0100

    Add first cypress test with locators

---
+++ cypress/e2e/learning/locator.cy.js
@@ -0,0 +1,11 @@
+/// <reference types="cypress" />
+
+describe("learn about locators", () => {
+    it("visit google home page", () => {
+        cy.visit("https://google.com");
+    });
+
+    it("can locate a button on a page", () => {
+        cy.get('.gLFyf');
+    });
+});
