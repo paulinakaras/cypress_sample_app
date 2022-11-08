6d172debcd11cc5205ef30cf586a2f99d5942fca
Paulina Karaś <ppaulina.karas@gmail.com>
2022-11-08 22:04:56 +0100

    Add test verifying google search dropdownn list

--- cypress/e2e/learning/02_interaction.cy.js
+++ cypress/e2e/learning/02_interaction.cy.js
@@ -20,4 +20,18 @@ describe("interacting with input field", () => {
         cy.get('@acceptAllButton').click();
         cy.get('@searchInput').click().focused();
     });
+
+    it("click on input field and see search suggestion dropdow", () => {
+        cy.get('#L2AGLb > .QS5gu').as("acceptAllButton");
+        cy.get('.gLFyf').as("searchInput");
+
+        cy.get('@acceptAllButton').click();
+        cy.get('@searchInput').click().focused();
+
+        cy.get("@searchInput").type('dontWant');
+        cy.get('.erkvQe > .OBMEnb > ul').as('dropdown');
+        cy.get('@dropdown').should('be.visible');
+        cy.get('@dropdown').contains('I Don\'t Want to Talk About It');
+        cy.get('@dropdown').find('li').eq(1).contains('I Don\'t Want to Talk About It');
+    })
 });
