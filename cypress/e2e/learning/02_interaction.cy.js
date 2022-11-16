d6a8e2f51b12fc7f911edc2ccb5e461e0baf3cae
Paulina Karaś <ppaulina.karas@gmail.com>
2022-11-16 22:27:16 +0100

    Add test with pressing enter button and sample skipping test

--- cypress/e2e/learning/02_interaction.cy.js
+++ cypress/e2e/learning/02_interaction.cy.js
@@ -21,7 +21,7 @@ describe("interacting with input field", () => {
         cy.get('@searchInput').click().focused();
     });
 
-    it("click on input field and see search suggestion dropdow", () => {
+    it("click on input field, type some text and click on the second result from search dropdown list", () => {
         cy.get('#L2AGLb > .QS5gu').as("acceptAllButton");
         cy.get('.gLFyf').as("searchInput");
 
@@ -33,5 +33,23 @@ describe("interacting with input field", () => {
         cy.get('@dropdown').should('be.visible');
         cy.get('@dropdown').contains('I Don\'t Want to Talk About It');
         cy.get('@dropdown').find('li').eq(1).contains('I Don\'t Want to Talk About It');
+
+        cy.get('@dropdown').find('li').eq(1).click();
+        cy.contains('I Don\'t Want to Talk About It');
+    });
+
+    it("click on input field, type some text and tap enter button", () => {
+        cy.get('#L2AGLb > .QS5gu').as("acceptAllButton");
+        cy.get('.gLFyf').as("searchInput");
+
+        cy.get('@acceptAllButton').click();
+        cy.get('@searchInput').click().focused();
+
+        cy.get("@searchInput").type('dontWant {enter}');
+        cy.contains('I Don\'t Want to');
+    });
+
+    it.skip("skipping test", () => {
+        // skipping test
     })
 });
